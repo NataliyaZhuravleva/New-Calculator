176be035d3824aee92005b105aa2ba82cb7ee213
Nataliya Zhuravleva <natalindria@gmail.com>
2020-10-23 00:12:23 -0700

    Add branching for all operation logic

--- js/scripts.js
+++ js/scripts.js
@@ -24,16 +24,18 @@ $(document).ready(function() {
     const number1 = parseInt($("#input1").val());
     const number2 = parseInt($("#input2").val());
     const operator = $("input:radio[name=operator]:checked").val();
-    console.log("1st number: " + number1);  // for debugging
-    console.log("2nd number: " + number2);  // for debugging
-    console.log("operator: " + operator);  // for debugging
-    const result = add(number1, number2);
+    
+    let result;
+    if (operator === "add"){
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide"){
+      result = divide(number1,number2);
+    }
+    console.log ("operator:"+operator);
     $("#output").text(result);
   });
 });
-//const result1 = add(number1, number2);
-//const result2 = subtract(number1, number2);
-//const result3 = multiply(number1, number2);
-//const result4 = divide(number1,number2)
-//alert(result1, result2, result3, result4);
-//alert(result4);
