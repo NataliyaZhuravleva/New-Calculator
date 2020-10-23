aab806152357300994ce208c4f61b5d27e8455c3
Nataliya Zhuravleva <natalindria@gmail.com>
2020-10-22 23:58:00 -0700

    Add UI multiple options

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,39 @@
-function add(number1, number2) {
+// Business logic:
+
+const add = function(number1, number2) {
   return number1 + number2;
-}
+};
 
-function subtract(number1, number2) {
+const subtract = function(number1, number2) {
   return number1 - number2;
-}
+};
 
-function multiply(number1,number2){
-  return number1*number2;
-}
+const multiply = function(number1, number2) {
+  return number1 * number2;
+};
 
-function divide(number1, number2){
-  return number1/number2;
-}
+const divide = function(number1, number2) {
+  return number1 / number2;
+};
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result1 = add(number1, number2);
-const result2 = subtract(number1, number2);
-const result3 = multiply(number1, number2);
-const result4 = divide(number1,number2)
-alert(result1, result2, result3, result4);
-alert(result4);
+// Everything below this line is user interface logic:
+
+$(document).ready(function() {
+  $("form#calculator").submit(function() {
+    event.preventDefault();
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    console.log("1st number: " + number1);  // for debugging
+    console.log("2nd number: " + number2);  // for debugging
+    console.log("operator: " + operator);  // for debugging
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
+//const result1 = add(number1, number2);
+//const result2 = subtract(number1, number2);
+//const result3 = multiply(number1, number2);
+//const result4 = divide(number1,number2)
+//alert(result1, result2, result3, result4);
+//alert(result4);
